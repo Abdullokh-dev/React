007b7d1080e42b7773ed514155aeceab25d43873
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-19 17:40:19 +0300

    repetition of Updating(array, obj, nested: arr, obj)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import {useState} from "react";
-import NavBar from "./components/Navbar";
-import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(['Product1', 'Product2', 'Product3']);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: 'john'
+    }
+  });
+  const [pizza, setPizza] = useState({
+    name: 'Spicy Pepperoni',
+    toppings: ['mushroom']
+  })
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      { id: 1, title: 'Product 1', quantity: 1 },
+      { id: 2, title: 'Product 2', quantity: 1 },
+    ]
+  })
+
+  const updateGame = () => {
+    setGame({...game, player: {...game.player, name: 'Bob'} })
+  }
+
+  const updatePizza = () => {
+    setPizza({...pizza, toppings: [...pizza.toppings, 'cheese']})
+  }
+
+  const updateCart = () => {
+    setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item, quantity: item.quantity + 1} : item)})
+  }
 
   return (
     <>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+      {game.player.name} <button onClick={updateGame}>update game</button>
+      <br/> <hr/>
+
+      <h4>{pizza.name}</h4>
+
+      <ul>{pizza.toppings.map((topping) => <li key={topping}>{topping}</li>)}</ul>
+
+      <button onClick={updatePizza}>update pizza</button>
+      <br/> <hr/>
+
+      <h4>Carts</h4>
+
+      <ul>{cart.items.map((item) => <li key={item.id}>{item.title}: {item.quantity}</li>)}</ul>
+
+      <button onClick={updateCart}>update cart</button>
     </>
   )
 }
