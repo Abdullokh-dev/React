d475939ba952e69403ebef89802c2df008d0d0ef
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-19 16:07:51 +0300

    Simplifying Update Logic with Immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {produce} from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -7,7 +8,14 @@ function App() {
   ]);
 
   const handleClick = () => {
-    setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug))
+    // setBugs(bugs.map(bug => bug.id === 1 ? { ...bug, fixed: true } : bug))
+
+    // darft is a proxy of object that records changes we're gonna apply tp the bugs array.
+    // imagine draft is a copy of the bugs array
+    setBugs(produce(draft => {
+      const bug = draft.find(bug => bug.id === 1)
+      if (bug) bug.fixed = true
+    }))
   }
 
   return (
