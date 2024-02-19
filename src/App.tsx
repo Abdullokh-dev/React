50079d30996365024e8e147c3cbfd0c697eeab45
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-19 15:44:40 +0300

    Updating Nested Objects (...obj, prop: {...obj.prop, prop: val})

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import {useState} from "react";
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: "Americano",
-    price: 5,
-  });
+  // const [drink, setDrink] = useState({
+  //   title: "Americano",
+  //   price: 5,
+  // });
+  const [customer, setCustomer] = useState({
+    name: 'Abdullokh',
+    address: {
+      city: 'Kokand',
+      zipCode: 150500
+    }
+  })
 
-  const handleClick = () => {
-    // const newDrink = {
-    //   ...drink,
-    //   price: 6,
-    // }
+  // const handleClick = () => {
+  //   // const newDrink = {
+  //   //   ...drink,
+  //   //   price: 6,
+  //   // }
+  //
+  //   setDrink({ ...drink, price: 6 });
+  // };
 
-    setDrink({ ...drink, price: 6 });
+  const setNestedObj = () => {
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 150501 }
+    });
   };
 
   return (
     <>
-      {drink.price}<button onClick={handleClick}>click me</button>
+      {customer.address.zipCode}<button onClick={setNestedObj}>click me</button>
     </>
   )
 }
