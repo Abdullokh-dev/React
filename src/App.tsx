e2af5460901f6944b9e8e0753c41c08b0e4bd92b
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-20 10:15:30 +0300

    Build Form component(Submission, Accessing Input Fields) with useRef.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid et eveniet laboriosam quia soluta
-        temporibus. Adipisci architecto, asperiores at atque consequatur cum doloribus et fugiat illo illum ipsa, itaque
-        iusto nam neque nesciunt nisi nostrum quasi quia saepe sapiente soluta tempora, tempore temporibus totam ullam!
-        Atque autem cupiditate, dolorum est explicabo fuga iure molestiae molestias necessitatibus nihil porro quis quo
-        quod saepe tempora. A animi aperiam blanditiis, commodi culpa dolorum earum eligendi, eos eveniet ex explicabo
-        facilis impedit ipsam itaque libero minima neque nesciunt non officiis, praesentium repellat temporibus
-        voluptatum. Aut deserunt doloribus fugiat iure nisi, qui ratione tempora.
-      </ExpandableText>
+      <Form />
     </>
   )
 }
