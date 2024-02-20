f8cfe322969869779ee71b1cb3797882cf00e8ba
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-20 16:47:04 +0300

    Build ExpenseForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import {useState} from "react";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -10,11 +13,13 @@ function App() {
     {id: 3, description: 'ccc', amount: 30, category: 'Utilities'},
     {id: 4, description: 'ddd', amount: 40, category: 'Utilities'}
   ]);
-
   const visibleExpense = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses
 
   return (
     <>
+      <div className="mb-3">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
       </div>
