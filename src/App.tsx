0572647c415f6aba55428a13d21808245e22cbc7
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-21 10:18:33 +0300

    Fetch data with Axios(then(), catch)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import {useState} from "react";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState('');
-  const [expenses, setExpenses] = useState([
-    {id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
-    {id: 2, description: 'bbb', amount: 20, category: 'Utilities'},
-    {id: 3, description: 'ccc', amount: 30, category: 'Utilities'},
-    {id: 4, description: 'ddd', amount: 40, category: 'Utilities'}
-  ]);
-  const visibleExpense = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+      .catch(err => setError(err.message))
+  }, [])
 
   return (
     <>
-      <div className="mb-3">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
-      </div>
-      <ExpenseList expenses={visibleExpense} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
+      { error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
     </>
   )
 }
