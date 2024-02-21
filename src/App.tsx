fb74186f74e85e71319e8af055332b10af9ff352
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-21 12:31:19 +0300

    Show Loading Indicator (Spinner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios, {AxiosError, CanceledError} from "axios";
+import axios, {CanceledError} from "axios";
 import {useEffect, useState} from "react";
 
 interface User {
@@ -9,24 +9,31 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   useEffect( () => {
     const controller = new AbortController();
 
+    setLoading(true);
      axios
        .get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal})
-       .then(res => setUsers(res.data))
+       .then((res) => {
+         setUsers(res.data);
+         setLoading(false);
+       })
        .catch((err) => {
          if (err instanceof CanceledError) return;
-         setError(err.message)
-       });
+         setError(err.message);
+         setLoading(false);
+       })
 
      return () => controller.abort();
   }, [])
 
   return (
     <>
-      { error && <p className="text-danger">{error}</p>}
+      { error && <p className="text-danger">{error}</p> }
+      { isLoading && <div className="spinner-border"></div> }
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
